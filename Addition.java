786380003513ad9a974e4a31a9d7c719e30c2991
aleeasgar0404 <alee.asgar@gmail.com>
2026-04-16 08:53:23 +0000

    this is java addition program

---
+++ Addition.java
@@ -0,0 +1,15 @@
+public class Addition {
+    public static void main(String[] args) {
+        // Declare two variables
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Calculate the sum
+        int sum = num1 + num2;
+        
+        // Display the result
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Sum: " + sum);
+    }
+}
